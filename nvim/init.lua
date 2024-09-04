d8c122ace6ceaafb7861d96f855c38f67dc1bd7f
LunamNauta <152558189+LunamNauta@users.noreply.github.com>
2024-09-04 03:46:03 +0000

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,5 +5,6 @@ vim.o.number = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.autochdir = true
+vim.o.cmdheight = 0
 
 vim.o.clipboard = "unnamedplus"
--- nvim/lua/plugins/auto_completion.lua
+++ nvim/lua/plugins/auto_completion.lua
@@ -1,9 +1,9 @@
 return {{
 	"hrsh7th/nvim-cmp",
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp", 
-		"hrsh7th/cmp-buffer", 
-		"hrsh7th/cmp-path", 
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
 
 		"L3MON4D3/LuaSnip",
@@ -28,7 +28,7 @@ return {{
 						fallback()
 					end
 				end, {"i", "s"}),
-				["<CR>"] = cmp.mapping.confirm({select = true})
+				["<CR>"] = cmp.mapping.confirm({select = true, behavior = cmp.ConfirmBehavior.Replace})
 			}),
 			sources = cmp.config.sources({
 				{name = "nvim_lsp"},
