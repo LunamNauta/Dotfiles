575ea7b57156ba51012525d3cb5ee6f193818de9
= <ayden.kranz@gmail.com>
2024-10-05 01:00:59 -0400

    Update from 'UploadConfig' (2024-10-05 01:00:59)

--- lua/Settings/Keybinds.lua
+++ lua/Settings/Keybinds.lua
@@ -7,3 +7,5 @@ vim.keymap.set("n", "<LEADER>distran", "<CMD>TransparentDisable<CR>")
 vim.keymap.set("n", "<LEADER>tn", "<CMD>tabnew<CR>")
 vim.keymap.set("n", "<LEADER>tk", "<CMD>bdelete<CR>")
 vim.keymap.set("n", "gt", function() vim.cmd("BufferGoto " .. vim.v.count) end)
+
+vim.keymap.set("n", "<LEADER>di", vim.lsp.buf.hover)
--- lua/Settings/Languages.lua
+++ lua/Settings/Languages.lua
@@ -2,7 +2,9 @@ local languages = {}
 languages.treesitter = {"cpp", "c", "lua", "python", "rust"}
 languages.mason_lsp = {"clangd", "lua_ls", "pylsp", "rust_analyzer"}
 languages.mason_lsp_opts = {
-	["clangd"] = {},
+	--If on windows, mason-lspconfig's clangd cannot find MSYS2 headers
+	--This 'cmd' is needed in order to locate headers in my build system
+	["clangd"] = {cmd = {"C:\\msys64\\ucrt64\\bin\\clangd.exe"}},
 	["lua_ls"] = {},
 	["pylsp"] = {},
 	["rust_analyzer"] = {}
